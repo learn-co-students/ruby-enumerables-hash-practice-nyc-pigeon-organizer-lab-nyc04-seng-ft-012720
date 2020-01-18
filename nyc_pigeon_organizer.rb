afb291277a13914badc049f9e6956d025e6683fe
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-18 04:46:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  birds = {}
+  data.each do |key, value|
+    p data
+    value.each do |new_value, bird_names|
+      p value
+      bird_names.each do |name|
+        if !birds[name]
+          birds[name] = {}
+        end
+        if !birds[name][key]
+          birds[name][key] = []
+        end
+        birds[name][key] << new_value.to_s
+      end
+    end
+  end
+  return birds
 end
+
