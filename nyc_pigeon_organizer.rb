e01a97caa58d4d6dd00ce6f13c225dadb4616501
Firuz Afzalov <afzalov98@gmail.com>
2020-01-23 01:13:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_pigeon_data = {}
+
+data.each do |pigeon_attribute, hash_of_attribute_values|
+  hash_of_attribute_values.each do |attribute_name, pigeon_name|
+    pigeon_name.each do |names_of_array|
+      if !new_pigeon_data[names_of_array]
+      new_pigeon_data[names_of_array] = {pigeon_attribute => [attribute_name.to_s]}
+      elsif !new_pigeon_data[names_of_array][pigeon_attribute]
+      new_pigeon_data[names_of_array][pigeon_attribute] = [attribute_name.to_s]
+      else
+      new_pigeon_data[names_of_array][pigeon_attribute] << attribute_name.to_s
+      end
+    end
+  end
+end 
+
+new_pigeon_data
 end
